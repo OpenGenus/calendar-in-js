484ed84a033cc7009b34a832b554eff35327a1f7
Virag Kormoczy <vkormoczy@gmail.com>
2023-02-25 16:42:40 +0200

    Generate 12 tables with week days

--- index.js
+++ index.js
@@ -1,37 +1,15 @@
 import months from './modules/months.js';
 import daysOfWeek from './modules/daysOfWeek.js';
 
-// const previousArrowBtn = document.getElementById('previous');
-// const nextArrowBtn = document.getElementById('next');
 const yearSpan = document.getElementById('year-span');
 const monthSpan = document.getElementById('month-span');
-const weekRow = document.getElementById('week');
 const yearInput = document.getElementById('year');
-const monthInput = document.getElementById('month');
-//const currentMonth = new Date().getMonth();
 const currentYear = new Date().getFullYear();
 const currentDay = new Date().getDay();
 const currentDate = new Date().getDate();
 const daysContainer = document.getElementById('days');
-
-// Create the days of the week
-daysOfWeek.forEach((day) => {
-  const dayHeader = document.createElement('th');
-  dayHeader.textContent = day;
-  dayHeader.setAttribute('class', 'week');
-  weekRow.appendChild(dayHeader);
-});
-
-// Create month options
-// months.forEach((month) => {
-//   const monthOption = document.createElement('option');
-//   monthOption.textContent = month;
-//   monthOption.value = month;
-//   if (month === months[currentMonth]) {
-//     monthOption.selected = true;
-//   }
-//   monthInput.appendChild(monthOption);
-// });
+const mainContainer = document.getElementById('table');
+let year = currentYear;
 
 // Select a year
 yearInput.addEventListener('change', () => {
@@ -43,40 +21,33 @@ yearInput.addEventListener('change', () => {
 yearInput.value = currentYear;
 yearSpan.textContent = yearInput.value;
 
-// Select a month
-// monthInput.addEventListener('change', () => {
-//   monthSpan.textContent = monthInput.value;
-//   setDays();
-// });
+months.forEach((month) => {
+  const div = document.createElement('div');
+  div.innerHTML = ` <table>
+  <thead>
+    <tr>
+      <th class="year-display" colspan="7">
+        <p>
+          <span class="month-span">${month}</span>
+        </p>
+      </th>
+    </tr>
+    <tr id=${month} class="weeksContainer"></tr>
+  </thead>
+  <tbody class="daysContainer"></tbody>
+</table>`;
 
-// Set current month as default value
-//monthSpan.textContent = monthInput.value;
+  mainContainer.appendChild(div);
 
-// Get previous month
-// previousArrowBtn.addEventListener('click', () => {
-//   yearSpan.textContent =
-//     monthSpan.textContent === 'January'
-//       ? Number(yearSpan.textContent) - 1
-//       : yearSpan.textContent;
-//   monthSpan.textContent =
-//     monthSpan.textContent === 'January'
-//       ? months[11]
-//       : months[months.indexOf(monthSpan.textContent) - 1];
-//   setDays();
-// });
-
-// Get next month
-// nextArrowBtn.addEventListener('click', () => {
-//   yearSpan.textContent =
-//     monthSpan.textContent === 'December'
-//       ? Number(yearSpan.textContent) + 1
-//       : yearSpan.textContent;
-//   monthSpan.textContent =
-//     monthSpan.textContent === 'December'
-//       ? months[0]
-//       : months[months.indexOf(monthSpan.textContent) + 1];
-//   setDays();
-// });
+  // Create the days of the week
+  const weekRow = document.getElementById(`${month}`);
+  daysOfWeek.forEach((day) => {
+    const dayHeader = document.createElement('th');
+    dayHeader.textContent = day;
+    dayHeader.setAttribute('class', 'week');
+    weekRow.appendChild(dayHeader);
+  });
+});
 
 // Set days text
 const setDaysText = () => {
