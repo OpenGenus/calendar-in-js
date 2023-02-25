7d70bb984ac69525311d0a7d76d3434afcbf7ab7
Virag Kormoczy <vkormoczy@gmail.com>
2023-02-25 16:19:57 +0200

    Remove arrow buttons and month options

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import months from './modules/months.js';
 import daysOfWeek from './modules/daysOfWeek.js';
 
-const previousArrowBtn = document.getElementById('previous');
-const nextArrowBtn = document.getElementById('next');
+// const previousArrowBtn = document.getElementById('previous');
+// const nextArrowBtn = document.getElementById('next');
 const yearSpan = document.getElementById('year-span');
 const monthSpan = document.getElementById('month-span');
 const weekRow = document.getElementById('week');
 const yearInput = document.getElementById('year');
 const monthInput = document.getElementById('month');
-const currentMonth = new Date().getMonth();
+//const currentMonth = new Date().getMonth();
 const currentYear = new Date().getFullYear();
 const currentDay = new Date().getDay();
 const currentDate = new Date().getDate();
@@ -23,15 +23,15 @@ daysOfWeek.forEach((day) => {
 });
 
 // Create month options
-months.forEach((month) => {
-  const monthOption = document.createElement('option');
-  monthOption.textContent = month;
-  monthOption.value = month;
-  if (month === months[currentMonth]) {
-    monthOption.selected = true;
-  }
-  monthInput.appendChild(monthOption);
-});
+// months.forEach((month) => {
+//   const monthOption = document.createElement('option');
+//   monthOption.textContent = month;
+//   monthOption.value = month;
+//   if (month === months[currentMonth]) {
+//     monthOption.selected = true;
+//   }
+//   monthInput.appendChild(monthOption);
+// });
 
 // Select a year
 yearInput.addEventListener('change', () => {
@@ -44,39 +44,39 @@ yearInput.value = currentYear;
 yearSpan.textContent = yearInput.value;
 
 // Select a month
-monthInput.addEventListener('change', () => {
-  monthSpan.textContent = monthInput.value;
-  setDays();
-});
+// monthInput.addEventListener('change', () => {
+//   monthSpan.textContent = monthInput.value;
+//   setDays();
+// });
 
 // Set current month as default value
-monthSpan.textContent = monthInput.value;
+//monthSpan.textContent = monthInput.value;
 
 // Get previous month
-previousArrowBtn.addEventListener('click', () => {
-  yearSpan.textContent =
-    monthSpan.textContent === 'January'
-      ? Number(yearSpan.textContent) - 1
-      : yearSpan.textContent;
-  monthSpan.textContent =
-    monthSpan.textContent === 'January'
-      ? months[11]
-      : months[months.indexOf(monthSpan.textContent) - 1];
-  setDays();
-});
+// previousArrowBtn.addEventListener('click', () => {
+//   yearSpan.textContent =
+//     monthSpan.textContent === 'January'
+//       ? Number(yearSpan.textContent) - 1
+//       : yearSpan.textContent;
+//   monthSpan.textContent =
+//     monthSpan.textContent === 'January'
+//       ? months[11]
+//       : months[months.indexOf(monthSpan.textContent) - 1];
+//   setDays();
+// });
 
 // Get next month
-nextArrowBtn.addEventListener('click', () => {
-  yearSpan.textContent =
-    monthSpan.textContent === 'December'
-      ? Number(yearSpan.textContent) + 1
-      : yearSpan.textContent;
-  monthSpan.textContent =
-    monthSpan.textContent === 'December'
-      ? months[0]
-      : months[months.indexOf(monthSpan.textContent) + 1];
-  setDays();
-});
+// nextArrowBtn.addEventListener('click', () => {
+//   yearSpan.textContent =
+//     monthSpan.textContent === 'December'
+//       ? Number(yearSpan.textContent) + 1
+//       : yearSpan.textContent;
+//   monthSpan.textContent =
+//     monthSpan.textContent === 'December'
+//       ? months[0]
+//       : months[months.indexOf(monthSpan.textContent) + 1];
+//   setDays();
+// });
 
 // Set days text
 const setDaysText = () => {
